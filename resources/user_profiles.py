8d52ce44a3691b42691db1fb5e8c795801522995
Dan Seiders <dseiders333@gmail.com>
2020-12-14 00:59:32 -0500

    update user profile route working

--- resources/user_profiles.py
+++ resources/user_profiles.py
@@ -15,7 +15,7 @@ def get_all_profiles():
     except models.DoesNotExist:
         return jsonify(data={}, status={'code': 401, 'message': 'Error getting the resources'})
 
-@profile.route('/new', methods=['POST'])
+@profile.route('/', methods=['POST'])
 def create_profile():
     print(request)
     payload = request.get_json()
@@ -28,3 +28,21 @@ def create_profile():
     # print(model_to_dict(user_profile), 'model to dict')
     user_profile_dict = model_to_dict(user_profile)
     return jsonify(data=user_profile_dict, status={'code': 201, 'message': 'Sucess'})
+
+@profile.route('/<id>', methods=['PUT'])
+def edit_profile(id):
+    print(request)
+    print(id)
+    payload = request.get_json()
+    print(payload)
+    query = models.Profiles.update(**payload).where(models.Profiles.id==id)
+    print(query)
+    query.execute()
+    return jsonify(data=model_to_dict(models.Profiles.get_by_id(id)), status={'code': 200, 'message':'resource updated successfully'})
+    return 'hello'
+
+@profile.route('/<id>', methods=['DELETE'])
+def delete_profile(id):
+    query = models.Profiles.delete().where(models.Profiles.id==id)
+    query.execute()
+    return jsonify(data='resource successfully deleted', status={'code': 200, 'message': 'resource deleted successfully'})
